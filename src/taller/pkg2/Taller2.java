11b9a6a608ecd19109f097267061ff590af7552e
punts hola mundo <anthonnyc10@gmail.com>
2017-08-07 08:23:08 -0600

    Interfaza creada

--- src/taller/pkg2/Taller2.java
+++ src/taller/pkg2/Taller2.java
@@ -16,6 +16,8 @@ public class Taller2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Ventana a = new Ventana();
+        a.setVisible(true);
     }
     
 }
